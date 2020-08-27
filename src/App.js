45e4d55cf5404d5ac439ad7b668869ea3069332b
Andre <andremassey@gmail.com>
2020-08-26 18:03:41 -0700

    header design finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/header/header.component";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       {/* Header */}
       <Header />
-      {/* Cards */}
-      {/* Buttons below cards */}
-      {/* Chats Screen */}
-      {/* Individual chat screen */}
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <p>chat Page</p>
+          </Route>
+          <Route path="/">
+            <p>Home Page</p>
+          </Route>
+        </Switch>
+        {/* Cards */}
+        {/* Buttons below cards */}
+        {/* Chats Screen */}
+        {/* Individual chat screen */}
+      </Router>
     </div>
   );
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./header.styles.css";
+import ForumIcon from "@material-ui/icons/Forum";
+import PersonRoundedIcon from "@material-ui/icons/PersonRounded";
+import IconButton from "@material-ui/core/IconButton";
 
 function Header() {
   return (
-    <div>
-      <h1>Working!</h1>
+    <div className="header">
+      <IconButton>
+        <PersonRoundedIcon className="header_icon" fontSize="large" />
+      </IconButton>
+      <img
+        className="header_logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+        alt="tinder_logo"
+      />
+      <IconButton>
+        <ForumIcon className="header_icon" fontSize="large" />
+      </IconButton>
     </div>
   );
 }
